6b657f2e20647c845962f0b85ec6ad4c054832d1
Matthias Hollinger <matthias.hollinger@gmail.com>
2026-05-27 11:06:16 +0200

    Initial Commit

---
+++ Nexus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nexus
+//
+//  Created by Kapsch Hollinger on 27.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NexusTests/NexusTests.swift
@@ -0,0 +1,19 @@
+//
+//  NexusTests.swift
+//  NexusTests
+//
+//  Created by Kapsch Hollinger on 27.05.26.
+//
+
+import Testing
+@testable import Nexus
+
+struct NexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
